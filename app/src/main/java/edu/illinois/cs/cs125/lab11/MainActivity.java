3379d5cbc0a67230500ef034796d69b6a5540365
isaac <iwarren2@illinois.edu>
2018-11-18 19:04:56 -0600

    Started conversion

--- app/src/main/java/edu/illinois/cs/cs125/lab11/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab11/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.illinois.cs.cs125.lab11;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -18,10 +22,22 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.net.URI;
+
 /**
  * Main class for our UI design lab.
  */
+
 public final class MainActivity extends AppCompatActivity {
+    private static final int IMAGE_CAPTURE_REQUEST_CODE = 1;
+    private static final int READ_REQUEST_CODE = 42;
+    private Uri currentPhotoURI;
+    /** Current file that we are using for our image request. */
+    private boolean photoRequestActive = false;
+
+    /** Whether a current photo request is being processed. */
+    private File currentPhotoFile = null;
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab11:Main";
 
@@ -47,15 +63,16 @@ public final class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // Set up the queue for our API requests
         requestQueue = Volley.newRequestQueue(this);
 
         setContentView(R.layout.activity_main);
-        final Button button = findViewById(R.id.button_get_ip);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(final View v) {
-                startAPICall(ipEditText.getText().toString());
-            }
+
+        final Button openFile = findViewById(R.id.openFile);
+        openFile.setOnClickListener(v -> {
+            Log.d(TAG, "Open file button clicked");
+            startOpenFile();
         });
 
         responseTextView = findViewById(R.id.textView_response);
@@ -116,8 +133,36 @@ public final class MainActivity extends AppCompatActivity {
             String country = response.getString("country");
             String hostname = response.getString("hostname");
 
-            responseTextView.setText(hostname + "\n\n" + city + ", " + region + ", " + country);
+
 
         } catch (JSONException ignored) { }
+    }
+    private void startOpenFile() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode,
+                                final Intent resultData) {
+        if (resultCode != Activity.RESULT_OK) {
+            Log.w(TAG, "onActivityResult with code " + requestCode + " failed");
+            if (requestCode == IMAGE_CAPTURE_REQUEST_CODE) {
+                photoRequestActive = false;
+            }
+            return;
+        }
+        if (requestCode == READ_REQUEST_CODE) {
+            currentPhotoURI = resultData.getData();
+        } else if (requestCode == IMAGE_CAPTURE_REQUEST_CODE) {
+            currentPhotoURI = Uri.fromFile(currentPhotoFile);
+            photoRequestActive = false;
+        } else {
+            Log.w(TAG, "Unhandled activityResult with code " + requestCode);
+            return;
+        }
+        Log.d(TAG, "Photo selection produced URI " + currentPhotoURI);
+        responseTextView.setText(currentPhotoURI.toString());
     }
 }
