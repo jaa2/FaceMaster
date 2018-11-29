2c463649d044ed74e88db5ed8e6cc6cc140fa2ab
Isaac <iwarren2@illinois.edu>
2018-11-29 12:57:04 -0600

    Added javadocs and removed unused functions

--- app/src/main/java/edu/illinois/cs/cs125/lab11/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab11/MainActivity.java
@@ -1,66 +1,33 @@
 package edu.illinois.cs.cs125.lab11;
 
-
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.provider.MediaStore;
-import android.support.annotation.CallSuper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
-
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.apache.http.HttpException;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * Main class for our UI design lab.
  */
 
 public final class MainActivity extends AppCompatActivity {
+    /**Code to get image from file explorer. */
     private static final int IMAGE_CAPTURE_REQUEST_CODE = 1;
+    /**Code to read a file from the file system. */
     private static final int READ_REQUEST_CODE = 42;
-    /** Base64 encoded image to submit to API */
+    /** Base64 encoded image to submit to API. */
     private String encodedString;
+    /**Uri of current selected photo. */
     private Uri currentPhotoURI;
     /** Current file that we are using for our image request. */
     private boolean photoRequestActive = false;
@@ -97,7 +64,8 @@ public final class MainActivity extends AppCompatActivity {
         final Button getResult = findViewById(R.id.get_result);
         getResult.setOnClickListener(v -> {
             Log.d(TAG, "Open file button clicked");
-            new CallAPI(MainActivity.this).execute("https://api-us.faceplusplus.com/facepp/v3/detect", "gender,age,emotion,ethnicity,beauty", encodedString);
+            new CallAPI(MainActivity.this).execute("https://api-us.faceplusplus.com/facepp/v3/detect",
+                    "gender,age,emotion,ethnicity,beauty", encodedString);
         });
         final Button openFile = findViewById(R.id.openFile);
         openFile.setOnClickListener(v -> {
@@ -117,24 +85,8 @@ public final class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-
-    /**
-     * Handle the response from our IP geolocation API.
-     *
-     * @param response response from our IP geolocation API.
+    /** Start the process of opening a file.
      */
-    void apiCallDone(final JSONObject response) {
-        try {
-            Log.d(TAG, response.toString(2));
-            // Example of how to pull a field off the returned JSON object
-            Log.i(TAG, response.get("hostname").toString());
-
-            System.out.print(response);
-
-
-
-        } catch (JSONException ignored) { }
-    }
     private void startOpenFile() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
